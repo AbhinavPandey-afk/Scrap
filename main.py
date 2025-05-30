36906a143147747b60a2fa6d5eea1603f35e01d0
Dhruv-78 <dhruvdhuparr2005@gmail.com>
2025-05-31 03:01:10 +0530

    more best

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from functions import *
 import warnings
 warnings.filterwarnings('ignore')
 
-
+row = []
 investor_url = input("Enter Investor Site Link:")
 keywords = ("presentation", "earnings", "q1", "results")  # Add more if needed
 pdf_link = find_presentation_pdf(investor_url, keywords)
+row.append(pdf_link)
 if pdf_link:
     print("📄 PDF Link:", pdf_link)
     pdf_url = pdf_link  # Replace with actual URL
@@ -16,14 +17,16 @@ if pdf_link:
 
         # Run financial queries
         queries = [
-            "What is the total revenue reported? Provide exact value only",
-            "What is the net income reported? Provide exact value only",
+            "What is the name of the bank? Provide only the name no extra information.",
+            "What is the total revenue reported? Provide exact value only in the form - e.g. 1.0 Billion",
+            "What is the net income reported? Provide exact value only in the form - e.g. 1.0 Billion",
         ]
 
         for query in queries:
             response = query_deepseek(query, context)
             # print(response)
             answer = response['choices'][0]['message']['content']
+            row.append(answer)
             print(f"Q: {query}\nA: {answer}\n")
 
     finally:
@@ -34,6 +37,9 @@ else:
     print("No PDF Found!!")
 
 
+# Appending data in the file
+import csv 
 
-
-
+with open('data.csv', 'a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(row)  
